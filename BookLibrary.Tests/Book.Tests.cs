59e54df8d5e913c8c4af91980113cd8f5d4b845d
Alexandra Gushchenskova <skowa98@gmail.com>
2018-10-29 09:52:16 +0300

    Task1 is added

--- BookLibrary.Tests/Book.Tests.cs
+++ BookLibrary.Tests/Book.Tests.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BookLibrary.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class BookTests
     {
+        [TestCaseSource(typeof(DataForToStringTests))]
+        public string ToStringTests_Book(Book book)
+            => book.ToString();
+
+        [TestCaseSource(typeof(DataForToStringIFormattableTests))]
+        public string ToStringIFormattableTests_BookAndFormat(Book book, string format)
+            => book.ToString(format);
+
+        [TestCaseSource(typeof(DataForToStringIFormattableWithCultureTests))]
+        public string ToStringIFormattableTests_BookAndFormat(Book book, string format, IFormatProvider provider)
+            => book.ToString(format, provider);
+
+        [Test]
+        public void ToStringTests_WrongFormat_ThrowsFormatException()
+            => Assert.Throws<FormatException>(() =>
+                new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40).ToString("N"));
+    }
+
+    internal class DataForToStringTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new TestCaseData(new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40)).Returns("Book record: Jon Skeet, C# in Depth, 2019, Manning, 4, 900, 40,00 ₽");
+        }
+    }
+
+    internal class DataForToStringIFormattableTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+
+            yield return new TestCaseData(book, "P").Returns("Book record: Jon Skeet, C# in Depth, 2019, Manning");
+            yield return new TestCaseData(book, "Y").Returns("Book record: Jon Skeet, C# in Depth, 2019");
+            yield return new TestCaseData(book, "A").Returns("Book record: Jon Skeet, C# in Depth");
+            yield return new TestCaseData(book, "P").Returns("Book record: Jon Skeet, C# in Depth, 2019, Manning");
+            yield return new TestCaseData(book, "T").Returns("Book record: C# in Depth");
+            yield return new TestCaseData(book, "H").Returns("Book record: C# in Depth, 2019, Manning");
+            yield return new TestCaseData(book, "M").Returns("Book record: C# in Depth, 40,00 ₽");
+            yield return new TestCaseData(book, "E").Returns("Book record: Jon Skeet, C# in Depth, 4, 900");
+        }
+    }
+
+    internal class DataForToStringIFormattableWithCultureTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+
+            yield return new TestCaseData(book, "G", CultureInfo.GetCultureInfo("en-US")).Returns("Book record: Jon Skeet, C# in Depth, 2019, Manning, 4, 900, $40.00");
+            yield return new TestCaseData(book, "Y", CultureInfo.GetCultureInfo("en-US")).Returns("Book record: Jon Skeet, C# in Depth, 2019");
+            yield return new TestCaseData(book, "M", CultureInfo.GetCultureInfo("en-US")).Returns("Book record: C# in Depth, $40.00");
+        }
     }
 }
--- BookLibrary/Book.cs
+++ BookLibrary/Book.cs
@@ -1,13 +1,156 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace BookLibrary
 {
-    public class Book
+    /// <summary>
+    /// This is a book class.
+    /// </summary>
+    public class Book : IFormattable
     {
-        //TODO
+        /// <summary>
+        /// Initializes a new instance of book.
+        /// </summary>
+        /// <param name="title">
+        /// The title.
+        /// </param>
+        /// <param name="author">
+        /// The author.
+        /// </param>
+        /// <param name="year">
+        /// The publishing year.
+        /// </param>
+        /// <param name="publishingHouse">
+        /// The publishing house.
+        /// </param>
+        /// <param name="edition">
+        /// The edition.
+        /// </param>
+        /// <param name="pages">
+        /// The number of pages.
+        /// </param>
+        /// <param name="price">
+        /// The price.
+        /// </param>
+        public Book(string title, string author, int year, string publishingHouse, int edition, int pages, double price)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            PublishingHouse = publishingHouse;
+            Edition = edition;
+            Pages = pages;
+            Price = price;
+        }
+
+        /// <summary>
+        /// Gets title.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets author.
+        /// </summary>
+        public string Author { get; }
+
+        /// <summary>
+        /// Gets year.
+        /// </summary>
+        public int Year { get; }
+
+        /// <summary>
+        /// Gets publishing house.
+        /// </summary>
+        public string PublishingHouse { get; }
+
+        /// <summary>
+        /// Gets edition.
+        /// </summary>
+        public int Edition { get; }
+
+        /// <summary>
+        /// Gets pages.
+        /// </summary>
+        public int Pages { get; }
+
+        /// <summary>
+        /// Gets the price.
+        /// </summary>
+        public double Price { get; }
+
+        /// <summary>
+        /// Gets the instance of a book as a string.
+        /// </summary>
+        /// <returns>
+        /// The instance of a book as a string.
+        /// </returns>
+        public override string ToString()
+        {
+            return ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Gets the instance of a book as a string in special format.
+        /// </summary>
+        /// <param name="format">
+        /// The format in which the instance of a book is represented.
+        /// </param>
+        /// <returns>
+        /// The instance of a book as a string in special <paramref name="format"/>.
+        /// </returns>
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Gets the instance of a book as a string in special format according to special culture.
+        /// </summary>
+        /// <param name="format">
+        /// The format in which the instance of a book is represented. G is an overrided ToString.
+        /// </param>
+        /// <param name="formatProvider">
+        /// The format according to which some objects are formatted.
+        /// </param>
+        /// <returns>
+        /// The instance of a book as a string in special <paramref name="format"/> and <paramref name="formatProvider"/>.
+        /// </returns>
+        /// <exception cref="FormatException">
+        /// Thrown when <paramref name="format"/> is wrong.
+        /// </exception>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "G";
+            }
+
+            if (formatProvider == null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                    return $"Book record: {Author}, {Title}, {Year}, {PublishingHouse}, {Edition}, {Pages}, {Price.ToString("C", formatProvider)}";
+                case "P":
+                    return $"Book record: {Author}, {Title}, {Year}, {PublishingHouse}";
+                case "Y":
+                    return $"Book record: {Author}, {Title}, {Year}";
+                case "A":
+                    return $"Book record: {Author}, {Title}";
+                case "T":
+                    return $"Book record: {Title}";
+                case "H":
+                    return $"Book record: {Title}, {Year}, {PublishingHouse}";
+                case "M":
+                    return $"Book record: {Title}, {Price.ToString("C", formatProvider)}";
+                case "E":
+                    return $"Book record: {Author}, {Title}, {Edition}, {Pages}";
+                default: 
+                    throw new FormatException($"{nameof(format)} is not supported.");
+            }
+        }
     }
 }
