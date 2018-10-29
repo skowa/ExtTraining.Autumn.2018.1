02f6fbe059aebdf7fc07799c0e5c5bddfff26567
Alexandra Gushchenskova <skowa98@gmail.com>
2018-10-29 11:22:00 +0300

    Add Third task

--- StringExtension/Parser.cs
+++ StringExtension/Parser.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringExtension
 {
@@ -10,7 +6,41 @@ namespace StringExtension
     {
         public static int ToDecimal(this string source, int @base)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} is null");
+            }
+
+            if (@base > 16 || @base < 2)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(@base)} is out of range");
+            }
+
+            return ConvertToDecimal(source, @base);
+        }
+
+        private static int ConvertToDecimal(string source, int @base)
+        {
+            char[] numerals = source.ToCharArray();
+            string numeralsValues = "0123456789ABCDEF";
+
+            int result = 0;
+
+            checked
+            {
+                for (int i = 0; i < numerals.Length; i++)
+                {
+                    int index = numeralsValues.IndexOf(numerals[i].ToString(), StringComparison.OrdinalIgnoreCase);
+                    if (index >= @base || index == -1)
+                    {
+                        throw new ArgumentException($"{nameof(numerals)} is bad for {nameof(@base)}");
+                    }
+
+                    result += index * (int)Math.Pow(@base, numerals.Length - i - 1);
+                }
+            }
+            
+            return result;
         }
     }
 }
