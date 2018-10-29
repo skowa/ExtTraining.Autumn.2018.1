d5c8d8d2cf306d920014f2dca78ed843793ff752
Alexandra Gushchenskova <skowa98@gmail.com>
2018-10-29 11:18:35 +0300

    Task2 is added

--- BookExtension.Tests/BookFormatExtensionTests.cs
+++ BookExtension.Tests/BookFormatExtensionTests.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Globalization;
+using BookLibrary;
+using NUnit.Framework;
 
 namespace BookExtension.Tests
 {
+    [TestFixture]
     public class BookFormatExtensionTests
     {
+        [TestCaseSource(typeof(DataForToStringTests))]
+        public string ExtensionToStringTests_Book(string format, Book book)
+            => string.Format(new BookFormatExtension(), "{0:" + format +"}", book);
+
+        [TestCaseSource(typeof(DataForToStringWithProviderTests))]
+        public string ExtensionToStringTestsWithFormatProvider_Book(string format, Book book, IFormatProvider provider)
+            => string.Format(new BookFormatExtension(provider), "{0:" + format + "}", book);
+    }
+
+    internal class DataForToStringTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+            yield return new TestCaseData("Q", book).Returns($"Book record: Jon Skeet, C# in Depth, 2019, {book.Price.ToString("C", CultureInfo.CurrentCulture)}");
+            yield return new TestCaseData("A", book).Returns("Book record: Jon Skeet, C# in Depth");
+            yield return new TestCaseData("A", null).Returns("");
+        }
+    }
+
+    internal class DataForToStringWithProviderTests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+            yield return new TestCaseData("Q", book, CultureInfo.GetCultureInfo("en-US")).Returns($"Book record: Jon Skeet, C# in Depth, 2019, $40.00");
+        }
     }
 }
--- BookExtension/BookFormatExtension.cs
+++ BookExtension/BookFormatExtension.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using BookLibrary;
 
 namespace BookExtension
 {
-    public class BookFormatExtension
+    //To add our format we should add class, which implements IFormatProvider and ICustomProvider. Here we can see the decorator pattern.
+    public class BookFormatExtension : IFormatProvider, ICustomFormatter
     {
+        private readonly IFormatProvider _parent;
+
+        public BookFormatExtension() : this(CultureInfo.CurrentCulture) { }
+
+        public BookFormatExtension(IFormatProvider parent) => _parent = parent;
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null || format != "Q")
+            {
+                return string.Format(_parent, "{0:" + format + "}", arg);
+            }
+
+            Book book = arg as Book;
+
+            return $"Book record: {book?.Author}, {book?.Title}, {book?.Year}, {book?.Price.ToString("C", _parent)}";
+        }
     }
 }
